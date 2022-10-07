a9b23ed4df0ce15246ae63216cc64910b88b91c5
EmranSWE <mdemran.swe@gmail.com>
2022-10-07 22:25:08 +0600

    update

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors')
 require('dotenv').config()
 const port =process.env.PORT || 5000;
 const app=express();
 
 //middleware
-app.use(cors());
 app.use(express.json());
-
+app.use(cors());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cgqrjb7.mongodb.net/?retryWrites=true&w=majority`;
