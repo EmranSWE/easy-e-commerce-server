e5053533c89dd21918f81fb1261af0b7e94c01ed
EmranSWE <mdemran.swe@gmail.com>
2022-11-27 21:26:04 +0600

    fixed live site issue

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const cors = require('cors')
 require('dotenv').config()
 const port =process.env.PORT || 5000;
 const app=express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
 app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cgqrjb7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
